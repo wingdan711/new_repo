3b7a193b1969571c08a4ae3997a3ed50571792fb
Tim Wing Cheng <timwing.cheng@ceridian.com>
2021-05-22 23:43:14 -0400

    collection preview finished

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Directory from '../../components/directory/directory.component';
+
+import './homepage.style.scss';
+
+const HomePage = () =>(
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <Directory/>
+        </div>
+    </div>
+)
+
+export default HomePage;
