142776f6c4caf3824e082a22b04de386dda90ce2
LAPTOP-V7J2D4N6\wingd <wing.dan@gmail.com>
2021-05-30 01:31:11 -0400

    Create cart-item component

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { toggleCartHidden} from '../../redux/cart/cart.actions'
 import { ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg'
 
+
 import './cart-icon.styles.scss';
 
 const CartIcon = ({toggleCartHidden}) => (
