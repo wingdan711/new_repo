9e0af95a3d3dd0d9d0541fe5f4a7f37c62be9955
Tim Wing Cheng <timwing.cheng@ceridian.com>
2021-06-16 22:51:19 -0400

    Update stripe integration, added favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({price}) => {
+    const priceForStripe = price * 100;
+    const publishableKey = "pk_test_51J3Az6IuvINL4J9pC0zNfGlLbU9YEvrkcYis1aspYza4PmJ69DQNtqBGKFVEmisCwG89cyLjBvFiKpRJIdc5FWnD002wagWbAU";
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful');
+    }
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='CRWN Clothing'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    )
+}
+
+export default StripeCheckoutButton;
